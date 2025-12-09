debb0c1b2302e9ec5013193ef2d38981d5e1140d
std67122420315-crypto <std.67122420315@ubru.ac.th>
2025-12-09 10:36:29 +0700

    Add files via upload
    Signed-off-by: std67122420315-crypto <std.67122420315@ubru.ac.th>

---
+++ py.py
@@ -0,0 +1,21 @@
+from flask import flask
+
+flask(``name``)
+
+app =flask
+
+@app .route(name)
+
+det index(name):
+    
+    return f'HELLO WORLD'
+
+@app .route('/about')
+def adout():
+    return 'about page'
+
+
+if__name``==``main :
+    
+    app.run(debug= True)
+    
